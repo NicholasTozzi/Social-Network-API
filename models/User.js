0211b6cfaf3cbb05f7df76b9032f6e97ed1cd500
Tozzi <nicholastozzi@gmail.com>
2023-03-21 23:03:50 -0400

    added user schema

---
+++ models/User.js
@@ -0,0 +1,38 @@
+const { Schema, model } = require("mongoose");
+const reactionSchema = require("./Reaction");
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      match: /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/,
+    },
+    thoughts: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
